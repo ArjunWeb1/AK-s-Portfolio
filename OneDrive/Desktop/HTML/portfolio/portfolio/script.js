40d4902fe55239d29e92eb3eefa948f0c0fcbdce
ArjunWeb1 <arjunkashid47@gmail.com>
2024-06-22 23:45:48 +0530

    Added Successully

---
+++ OneDrive/Desktop/HTML/portfolio/portfolio/script.js
@@ -0,0 +1,18 @@
+// script.js
+
+function smoothScroll(targetId) {
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
+function downloadResume() {
+    // Replace 'path/to/your_resume.pdf' with the actual path to your resume PDF file
+    window.location.href = 'path/to/your_resume.pdf';
+}
+
+
